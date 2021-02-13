efbab5551d5b1f46e705adcf41b56b5472ff137f
hinok <Hinoko@mail.com>
2021-02-13 14:35:17 +0000

    Work Experience page update

--- Hinok.js
+++ Hinok.js
@@ -11,6 +11,21 @@ var topMenu = $("#Menu"),
       if (item.length) { return item; }
     });
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 $(function(){
   $('button').click(function(e){
     var test = document.getElementById("Menu");
